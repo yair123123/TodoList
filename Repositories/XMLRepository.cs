3a59ed724c066067ec9d11af64c15a495a8660cc
yair1 <yair1@YAIR-ACHRACK>
2024-07-22 10:26:15 +0300

    yair

--- Repositories/XMLRepository.cs
+++ Repositories/XMLRepository.cs
@@ -1,12 +1,25 @@
-﻿
+﻿using System.Xml.Linq;
+using System.Xml.Serialization;
+
+
 namespace TodoList.Repositories
 {
     internal class XMLRepository : IRepository<TodoModel>
     {
         public TodoModel Add(TodoModel todo)
         {
-            Console.WriteLine("21346");
-            throw new NotImplementedException();
+            todo.Id = 1;
+            XmlSerializer serializer = new XmlSerializer(typeof(TodoModel));
+            using (StreamWriter writer = new StreamWriter("todo.xml"))
+            {
+                serializer.Serialize(writer, todo);
+            }
+            XmlSerializer newserializer = new XmlSerializer(typeof(TodoModel));
+            using (StreamReader reader = new StreamReader("todo.xml"))
+            {
+                todo = (TodoModel)serializer.Deserialize(reader);
+            }
+            return todo;
         }
 
         public void DeleteById(int id)
--- Todos.cs
+++ Todos.cs
@@ -43,12 +43,10 @@ namespace TodoList
             }
         }
 
-        // add or edit based on mode
+
         private void button_action_Click(object sender, EventArgs e)
         {
-            // נסיון זה הערה שרייניץ רשם
-            //נסיון נוסף
-            //נסיון שלישי
+
         }
     }
 }
