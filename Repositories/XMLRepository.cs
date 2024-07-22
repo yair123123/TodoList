618622a26cd802577dfa8b6790035edfb6d1e3e6
yair1 <yair1@YAIR-ACHRACK>
2024-07-22 10:41:33 +0300

    yyy

--- Repositories/XMLRepository.cs
+++ Repositories/XMLRepository.cs
@@ -6,9 +6,11 @@ namespace TodoList.Repositories
 {
     internal class XMLRepository : IRepository<TodoModel>
     {
+        public int Id = 0;
+
         public TodoModel Add(TodoModel todo)
         {
-            todo.Id = 1;
+            todo.Id = Id+=1;
             XmlSerializer serializer = new XmlSerializer(typeof(TodoModel));
             using (StreamWriter writer = new StreamWriter("todo.xml"))
             {
